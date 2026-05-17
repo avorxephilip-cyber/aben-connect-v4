dcb96d7937dd08f30a7aa45e4cdaeab36407383b
avorxephilip-cyber <avorxephilip@gmail.com>
2026-05-17 05:46:41 +0000

    Create Home component for landing page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <main>
+            <h1>Aben Connect</h1>
+                  <p>E-commerce platform - Coming Soon</p>
+                      </main>
+                        )
+                        }
