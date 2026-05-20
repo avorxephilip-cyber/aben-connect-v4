9fed6dea22f21c3968215a2b05de0aa19906b5cd
avorxephilip-cyber <avorxephilip@gmail.com>
2026-05-20 07:57:57 +0000

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-      <main>
-            <h1>Aben Connect</h1>
-                  <p>E-commerce platform - Coming Soon</p>
-                      </main>
-                        )
-                        }
